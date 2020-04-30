87979e0a6d86f218bf2e7afafb070bb8b8060083
Sayantan Ghosh <sayantan.iitb@gmail.com>
2020-05-01 00:01:16 +0530

    Start working on a new script to handle duplication bugs

---
+++ src/vcf_generator/version3/vcf_generator_ver3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  29 19:23:56 2020
+
+@author: sayantan
+"""
+import time as tm
+import os, sys
+import twobitreader
+
+def main():
+    print("Start of code:", tm.ctime(tm.time()))
+
+    print("End of code:", tm.ctime(tm.time()))
+
+if __name__ == '__main__':
+    main(sys.argv[1])
