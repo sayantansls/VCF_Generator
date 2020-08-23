e8208a463fbc598c23f82f73dbdf0398f6954268
Sayantan Ghosh <sayantan.iitb@gmail.com>
2020-08-23 23:32:20 +0530

    Revert "Fixed issues in setup.sh script"
    This reverts commit 4f9bea029af4605cbc89b2398f417800c21fa071.

--- setup.sh
+++ setup.sh
@@ -1,27 +1,21 @@
 echo "INFO: Starting the setup for vcf-generator"
 echo "INFO: Checking scripts and files status"
 
-GENES_37="strandomics_input_data/GrCh37/genes.tsv"
-GENES_38="strandomics_input_data/GrCh38/genes_38.tsv"
-HG_19="human_genome_data/GrCh37/hg19.2bit"
-HG_38="human_genome_data/GrCh38/hg38.2bit"
-VCF_TEMPLATE="vcf_template/vcf_template.vcf"
+cd ../data/
 
-cd data/
-
-if [[ -f $GENES_37 ]]; then
+if [[ -f strandomics_input_data/GrCh37/genes.tsv ]]; then
 	echo "SUCCESS: GrCh37 genes.tsv file present"
 else
 	echo "FAIL: GrCh37 genes.tsv file absent"
 fi
 
-if [[ -f $GENES_38 ]]; then
-	echo "SUCCESS: GrCh38 genes_38.tsv file present"
+if [[ -f strandomics_input_data/GrCh37/genes.tsv ]]; then
+	echo "SUCCESS: GrCh38 genes38.tsv file present"
 else
-	echo "FAIL: GrCh38 genes_38.tsv file absent"
+	echo "FAIL: GrCh38 genes38.tsv file absent"
 fi
 
-if [[ -f $VCF_TEMPLATE ]]; then
+if [[ -f vcf_template/vcf_template.vcf ]]; then
 	echo "SUCCESS: VCF template file present"
 else
 	echo "FAIL: VCF template file absent"
@@ -29,28 +23,26 @@ fi
 
 echo "INFO: The binary human genome reference data to be downloaded"
 
-if [[ ! -f $HG_19 ]]; then
-	echo "INFO: Begining the download of GrCh37 data - hg19.2bit....."
-	wget http://hgdownload.cse.ucsc.edu/goldenpath/hg19/bigZips/hg19.2bit
-	echo "INFO: Download of GrCh37 data - hg19.2bit ends"
-	mv hg19.2bit human_genome_data/GrCh37/
-fi
+echo "INFO: Begining the download of GrCh37 data - hg19.2bit....."
+wget http://hgdownload.cse.ucsc.edu/goldenpath/hg19/bigZips/hg19.2bit
+echo "INFO: Download of GrCh37 data - hg19.2bit ends"
 
-if [[ -f $HG_19 ]]; then
-	echo "SUCCESS: hg19.2bit file downloaded and present in appropriate location"
+mv hg19.2bit human_genome_data/GrCh37/
+
+if [[ -f human_genome_data/GrCh37/hg19.2bit ]]; then
+	echo "SUCCESS: hg19.2bit file downloaded and placed in appropriate location"
 else
 	echo "FAIL: hg19.2bit file absent"
 fi
 
-if [[ ! -f $HG_38 ]]; then
-	echo "INFO: Begining the download of GrCh38 data - hg38.2bit....."
-	wget http://hgdownload.cse.ucsc.edu/goldenpath/hg38/bigZips/hg38.2bit
-	echo "INFO: Download of GrCh38 data - hg38.2bit ends"
-	mv hg38.2bit human_genome_data/GrCh38/
-fi
+echo "INFO: Begining the download of GrCh38 data - hg38.2bit....."
+wget http://hgdownload.cse.ucsc.edu/goldenpath/hg38/bigZips/hg38.2bit
+echo "INFO: Download of GrCh38 data - hg38.2bit ends"
 
-if [[ -f $HG_38 ]]; then
-	echo "SUCCESS: hg38.2bit file downloaded and present in appropriate location"
+mv hg38.2bit human_genome_data/GrCh38/
+
+if [[ -f human_genome_data/GrCh38/hg38.2bit ]]; then
+	echo "SUCCESS: hg38.2bit file downloaded and placed in appropriate location"
 else
 	echo "FAIL: hg38.2bit file absent"
 fi
