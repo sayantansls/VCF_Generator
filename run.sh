02452d893161abdf36566aeb8140d6fc0fea8d9b
Sayantan Ghosh <sayantan.iitb@gmail.com>
2020-09-24 18:27:58 +0530

    run.sh mapped to version4

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-cd src/vcf_generator/version3/
-PYTHONPATH=../../common/ python vcf_generator_ver3.py
+if [[ ! -f vcf_generator_ver4.py ]]; then
+	echo "ERROR : vcf_generator_ver4.py script is not present"
+	exit 0
+fi
 
-echo 'INFO: output file present at location - data/output/'
+PYTHONPATH=../../common python3 vcf_generator_ver4.py $@
